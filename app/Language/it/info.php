c271dea320af7ae72fdd915635fc3c7981876d91
saloniamatteo <saloniamatteo@pm.me>
2024-05-06 20:06:34 +0200

    Translations (it): Shorten 'info'

--- app/Language/it/info.php
+++ app/Language/it/info.php
@@ -4,7 +4,7 @@ return [
 	"title"			=> "Informazioni",
 	"desc"			=> "Informazioni su Matteo Salonia.",
 	"welcome"		=> [
-		"title"		=> "Informazioni",
+		"title"		=> "Info",
 		"desc"		=> "Scopri chi sono, le mie competenze, e le mie conoscenze.",
 	],
 	"hero"			=> [
