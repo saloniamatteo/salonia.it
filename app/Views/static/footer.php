4e350b96cd2d145331206356f6996b1175d3e73a
saloniamatteo <saloniamatteo@pm.me>
2024-06-23 16:44:04 +0200

    Update footer

--- app/Views/static/footer.php
+++ app/Views/static/footer.php
@@ -1,5 +1,5 @@
 <!-- Footer -->
-<footer class="footer bg-blue-700 px-4 pt-6 pb-4 mt-0 u-shadow-inset">
-	<h5 class="title text-white pt-2 mb-1">Matteo Salonia</h5>
-	<p class="font-bold text-white mb-0">&copy; 2022-<?= date("Y") ?></p>
+<footer class="footer bg-blue-300 px-4 pt-6 pb-4 mt-0" style="border-top: 1px solid black">
+	<h5 class="title text-black pt-2 mb-1">Matteo Salonia</h5>
+	<p class="font-bold text-black mb-0">&copy; 2022-<?= date("Y") ?></p>
 </footer>
