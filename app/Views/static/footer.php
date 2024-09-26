43077746fce11cfef851f20b8df53efd3a371862
saloniamatteo <saloniamatteo@pm.me>
2024-09-26 21:43:33 +0200

    Fix logo size, add link

--- app/Views/static/footer.php
+++ app/Views/static/footer.php
@@ -1,10 +1,10 @@
 <!-- Footer -->
 <footer class="footer bg-black px-4 pt-4 pb-4 mt-0 u-center u-shadow-inset">
 	<a href="https://salonia.it">
-		<img src="<?= base_url('pics/saloniaw.png') ?>" alt="Logo" class="w-24" style="min-width: 180px">
+		<img src="<?= base_url('pics/saloniaw.png') ?>" alt="Logo" class="h-4" style="min-width: 180px">
 	</a>
 
 	<h4 class="title text-white mt-1 mb-1" style="padding-top: 0.15em !important">Infrastrutture Digitali</h4>
-	<h6 class="font-bold text-white mb-0 w-100p">di <span class="text-blue-300">Salonia Matteo</span></h6>
+	<h6 class="font-bold text-white mb-0 w-100p">di <a class="text-blue-300" href="https://salonia.it/contact">Salonia Matteo</a></h6>
 	<p class="font-bold text-white mb-0 w-100p">&copy; 2022-<?= date("Y") ?></p>
 </footer>
