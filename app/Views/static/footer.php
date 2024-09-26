f345708cfe67306079db925dcebf05f593658261
saloniamatteo <saloniamatteo@pm.me>
2024-09-26 21:35:06 +0200

    Update footer, make image smaller

--- app/Views/static/footer.php
+++ app/Views/static/footer.php
@@ -1,5 +1,10 @@
 <!-- Footer -->
-<footer class="footer bg-blue-700 px-4 pt-6 pb-4 mt-0 u-shadow-inset" style="border-top: 1px solid black">
-	<h5 class="title text-white pt-2 mb-1">Matteo Salonia</h5>
-	<p class="font-bold text-white mb-0">&copy; 2022-<?= date("Y") ?></p>
+<footer class="footer bg-black px-4 pt-4 pb-4 mt-0 u-center u-shadow-inset">
+	<a href="https://salonia.it">
+		<img src="<?= base_url('pics/saloniaw.png') ?>" alt="Logo" class="w-24" style="min-width: 180px">
+	</a>
+
+	<h4 class="title text-white mt-1 mb-1" style="padding-top: 0.15em !important">Infrastrutture Digitali</h4>
+	<h6 class="font-bold text-white mb-0 w-100p">di <span class="text-blue-300">Salonia Matteo</span></h6>
+	<p class="font-bold text-white mb-0 w-100p">&copy; 2022-<?= date("Y") ?></p>
 </footer>
