749eeed40561efd97de7906b160d725ab158d6c2
saloniamatteo <saloniamatteo@pm.me>
2024-09-26 21:46:04 +0200

    Fix logo sizing

--- app/Views/static/footer.php
+++ app/Views/static/footer.php
@@ -1,7 +1,7 @@
 <!-- Footer -->
 <footer class="footer bg-black px-4 pt-4 pb-4 mt-0 u-center u-shadow-inset">
 	<a href="https://salonia.it">
-		<img src="<?= base_url('pics/saloniaw.png') ?>" alt="Logo" class="h-4" style="min-width: 180px">
+		<img src="<?= base_url('pics/saloniaw.png') ?>" alt="Logo" style="min-width: 180px; width: 14rem !important">
 	</a>
 
 	<h4 class="title text-white mt-1 mb-1" style="padding-top: 0.15em !important">Infrastrutture Digitali</h4>
