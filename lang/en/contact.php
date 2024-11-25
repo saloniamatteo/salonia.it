4b3d49b32f15c50869663ad16be4d824aaa458cd
saloniamatteo <saloniamatteo@pm.me>
2024-11-25 16:58:46 +0100

    Lang: Add contact dicts

---
+++ lang/en/contact.php
@@ -0,0 +1,25 @@
+<?php
+
+// English dict file for contact
+return [
+	"title" => "Contact me",
+	"desc" => "Discover how to contact me.",
+
+	// Salonia Infrastrutture Digitali
+	"sid" => [
+		"ig" => "Instagram Business Profile",
+		"threads" => "Threads Business Profile",
+		"fb" => "Facebook Business Profile",
+		"maps" => "Find us on Google Maps",
+		"email" => "Business Email",
+	],
+
+	// Me
+	"me" => [
+		"title" => "Personal",
+
+		"gpg" => "GPG key",
+		"pm" => "Contact me via ProtonMail",
+		"pserv" => "Contact me via my email server",
+	],
+];
