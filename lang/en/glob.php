335ed31ecb9ad31a24631bda45d37def95e48614
saloniamatteo <saloniamatteo@pm.me>
2024-11-27 15:34:37 +0100

    Update global translations

--- lang/en/glob.php
+++ lang/en/glob.php
@@ -2,7 +2,11 @@
 
 // Global English strings
 return [
+	// Navigation
 	"go-to" => "Go to",
 	"to-main" => "&larr; Back to the main page",
+
+	// Cards
+	"info" => "Info",
 	"warning" => "Warning",
 ];
--- lang/it/glob.php
+++ lang/it/glob.php
@@ -2,7 +2,11 @@
 
 // Global Italian strings
 return [
+	// Navigation
 	"go-to" => "Vai a",
 	"to-main" => "&larr; Torna alla pagina principale",
+
+	// Cards
+	"info" => "Info",
 	"warning" => "Attenzione",
 ];
