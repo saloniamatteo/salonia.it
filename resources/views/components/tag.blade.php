32282ca1c7216e249ee95e4719dc624e58900a6d
saloniamatteo <saloniamatteo@pm.me>
2025-01-03 17:57:59 +0100

    Test: make tag smaller

--- resources/views/components/tag.blade.php
+++ resources/views/components/tag.blade.php
@@ -1,4 +1,4 @@
-<h3 class="tag tag--lg bg-blue-700 my-0" id="{{ $id }}">
+<h3 class="tag tag--lg bg-blue-700 my-0" id="{{ $id }}" style="font-size: 1.1rem">
     <a class="text-white" href="#{{ $id }}">
         {{ $slot }}
     </a>
