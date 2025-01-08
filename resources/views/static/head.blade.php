5223f9832e60d98eb38f5b68757d41a16084805f
saloniamatteo <saloniamatteo@pm.me>
2025-01-08 22:49:51 +0100

    Add & Reorder OG tags

--- resources/views/static/head.blade.php
+++ resources/views/static/head.blade.php
@@ -12,26 +12,25 @@
 
 	<!-- SEO (do not include if "noseo" is set) -->
 	@if (!isset($noseo))
-	<!-- Description -->
-	<meta name="description"                content="{{ $desc ?? 'Desc' }}">
-	<meta property="og:description"         content="{{ $desc ?? 'Desc' }}">
-	<meta property="twitter:description"    content="{{ $desc ?? 'Desc' }}">
+	<meta name="description" content="{{ $desc ?? 'Desc' }}">
 
-	<!-- Title -->
-	<meta property="og:title"               content="{{ $title ?? 'Hello, world!'}}">
-	<meta property="twitter:title"          content="{{ $title ?? 'Hello, world' }}">
-	<meta property="og:url"                 content="{{ $url ?? Url::getURL() }}">
+	<!-- OpenGraph (Facebook) tags -->
+	<meta property="og:title"        content="{{ $title ?? 'Hello, world!'}}">
+	<meta property="og:description"  content="{{ $desc ?? 'Desc' }}">
+	<meta property="og:site_name"    content="{{ Config::get('app.sitename') }}">
+	<meta property="og:image"        content="/banner.png">
+	<meta property="og:image:alt"    content="{{ Config::get('app.name') }}">
+	<meta property="og:image:height" content="600">
+	<meta property="og:image:width"  content="1200">
+	<meta property="og:type"         content="website">
+	<meta property="og:url"          content="{{ $url ?? Url::getURL() }}">
 
-	<!-- Banner image -->
-	<meta property="og:image:alt"           content="{{ Config::get('app.name') }}">
-	<meta property="og:image"               content="/banner.png">
-	<meta property="twitter:image"          content="/banner.png">
-	<meta property="og:image:width"         content="1200">
-	<meta property="og:image:height"        content="600">
-
-	<!-- Misc -->
-	<meta property="og:site_name"           content="{{ Config::get('app.sitename') }}">
-	<meta property="og:type"                content="website">
+	<!-- Twitter/X tags -->
+	<meta name="twitter:card"            content="summary_large_image">
+	<meta property="twitter:title"       content="{{ $title ?? 'Hello, world' }}">
+	<meta property="twitter:description" content="{{ $desc ?? 'Desc' }}">
+	<meta property="twitter:image"       content="/banner.png">
+	<meta property="twitter:url"         content="{{ $url ?? Url::getURL() }}">
 	@endif
 
 	<!-- Favicon -->
