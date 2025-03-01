23018ad5c21da404dae27102dfab5b43aa266c4b
saloniamatteo <saloniamatteo@pm.me>
2025-03-01 18:34:36 +0100

    Laravel Envoy: add composer command

--- EnvoyExample.blade.php
+++ EnvoyExample.blade.php
@@ -1,9 +1,10 @@
 // Laravel Envoy example file. Rename this to "Envoy.blade.php"!
-
+// Run with "composer deploy".
+// ---
 // NOTE: change 'user@127.0.0.1' with your remote server!
 @servers(['web' => 'user@127.0.0.1'])
 
-@task('update', ['on' => 'web'])
+@task('deploy', ['on' => 'web'])
     cd /var/www/salonia.it
     git pull
     scripts/update.sh
