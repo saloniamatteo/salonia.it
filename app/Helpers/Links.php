9568498ac7460066ca6abd2633a0001e1ef4cff2
saloniamatteo <saloniamatteo@pm.me>
2025-06-03 15:02:53 +0200

    Helpers/Links: add missing links

--- app/Helpers/Links.php
+++ app/Helpers/Links.php
@@ -6,64 +6,80 @@ use App\Helpers\Url;
 
 class Links
 {
-	public const maps = [
-		'writeups' => 'notebook-pen',
-		'kernel' => 'server-cog',
-		'packages' => 'package',
-		'software' => 'binary',
-		'services' => 'server',
-		'cv' => 'file-badge',
-		'info' => 'user-round',
-		'contact' => 'send',
-		'donate' => 'hand-coins',
-	];
+    public const MAPS = [
+        'arch' => [
+            'link' => 'https://arch.salonia.it',
+            'icon' => 'book-open-text'
+        ],
 
-	public const link_maps = [
-		'portfolio' => [
-			'link' => 'https://portfolio.salonia.it',
-			'icon' => 'globe',
-		],
+        'dotfiles' => [
+            'link' => 'https://dotfiles.salonia.it',
+            'icon' => 'folder-cog'
+        ],
 
-		'searxng' => [
-			'link' => 'https://s.salonia.it',
-			'icon' => 'search',
-		],
+        'writeups' => 'notebook-pen',
+        'kernel' => 'server-cog',
+        'packages' => 'package',
+        'software' => 'binary',
+        'services' => 'server',
+        'cv' => 'file-badge',
+        'info' => 'user-round',
+        'contact' => 'send',
+        'donate' => 'hand-coins',
+    ];
 
-		'oa' => [
-			'link' => 'https://oa.salonia.it',
-			'icon' => 'scroll-text',
-		],
+    public const LINK_MAPS = [
+        'portfolio' => [
+            'link' => 'https://portfolio.salonia.it',
+            'icon' => 'globe',
+        ],
 
-		'github' => [
-			'link' => 'https://github.com/saloniamatteo',
-			'icon' => 'github',
-		],
+        'searxng' => [
+            'link' => 'https://s.salonia.it',
+            'icon' => 'search',
+        ],
 
-		'source' => [
-			'link' => 'https://github.com/saloniamatteo/salonia.it',
-			'icon' => 'github',
-		],
-	];
+        'oa' => [
+            'link' => 'https://oa.salonia.it',
+            'icon' => 'scroll-text',
+        ],
 
-	public static function getPagesLink($key)
-	{
-		return $key === 'cv'
-				? Url::getCVLink()
-				: Url::subUrl("{$key}");
-	}
+        'github' => [
+            'link' => 'https://github.com/saloniamatteo',
+            'icon' => 'github',
+        ],
 
-	public static function getPagesIcon($key)
-	{
-		return self::maps["{$key}"];
-	}
+        'source' => [
+            'link' => 'https://github.com/saloniamatteo/salonia.it',
+            'icon' => 'github',
+        ],
+    ];
 
-	public static function getLink($key)
-	{
-		return self::link_maps["{$key}"]["link"];
-	}
+    public static function getPagesLink($key)
+    {
+        return $key === 'cv'
+                ? Url::getCVLink()
+                : (
+                    isset(self::MAPS["{$key}"]["link"])
+                    ? self::MAPS["{$key}"]["link"]
+                    : Url::subUrl("{$key}")
+                );
+    }
 
-	public static function getIcon($key)
-	{
-		return self::link_maps["{$key}"]["icon"];
-	}
+    public static function getPagesIcon($key)
+    {
+        return isset(self::MAPS["{$key}"]["icon"])
+               ? self::MAPS["{$key}"]["icon"]
+               : self::MAPS["{$key}"];
+    }
+
+    public static function getLink($key)
+    {
+        return self::LINK_MAPS["{$key}"]["link"];
+    }
+
+    public static function getIcon($key)
+    {
+        return self::LINK_MAPS["{$key}"]["icon"];
+    }
 }
