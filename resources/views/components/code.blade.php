ec2f4ae7f35ffa71638f92c257c07b0179ffa882
saloniamatteo <saloniamatteo@pm.me>
2025-06-05 19:43:02 +0200

    Code: remove border-left

--- resources/views/components/code.blade.php
+++ resources/views/components/code.blade.php
@@ -18,5 +18,5 @@
     </div>
 
     <!-- Code -->
-    <pre><code class="w-100p p-2 pt-6 text-md shj-lang-{{ $lang }}" {!! $attr !!}>{!! $slot !!}</code></pre>
+    <pre><code style="border-left: 0" class="w-100p p-2 pt-6 text-md shj-lang-{{ $lang }}" {!! $attr !!}>{!! $slot !!}</code></pre>
 </div>
