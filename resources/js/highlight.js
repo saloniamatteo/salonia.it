10d0034c2faab7ea617f718a82ad513e53b3b813
saloniamatteo <saloniamatteo@pm.me>
2025-06-05 19:30:07 +0200

    Code: show visual confirmation on copy

--- resources/js/highlight.js
+++ resources/js/highlight.js
@@ -4,12 +4,33 @@ import { highlightElement } from '@speed-highlight/core';
 // Clipboard JS
 import Clipboard from 'clipboard';
 
+// Sleep
+import { sleep } from './sleep.js';
+
 // Style
 import '../../node_modules/@speed-highlight/core/dist/themes/atom-dark.css';
+
+async function confirmClick(ele, text) {
+    // Show a checkmark
+    ele.innerText = ' ✅ OK! ';
+
+    // Wait 1 second
+    await sleep(1000);
+
+    // Show original text
+    ele.innerText = text;
+}
 
 // Initialize Clipboard.js
 var clipboard = new Clipboard('.copyButton', {
     text: function(trigger) {
+        // Get span text
+        let span = trigger.querySelector("span");
+        let span_text = span.innerText;
+
+        // Confirm user click
+        confirmClick(span, span_text);
+
         // Get text from parent node -> pre code -> (inner text)
         return trigger.parentNode.querySelector('pre code').innerText;
     }
