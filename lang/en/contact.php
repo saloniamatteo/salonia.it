3b1b7221b1e726f8ed052fbaeab52adedeeb76fb
saloniamatteo <saloniamatteo@pm.me>
2025-06-16 17:38:55 +0200

    Remove services

--- lang/en/contact.php
+++ lang/en/contact.php
@@ -5,19 +5,8 @@ return [
     'title' => 'Contact me',
     'desc' => 'Discover how to contact me.',
 
-    // Salonia Infrastrutture Digitali
-    'sid' => [
-        'ig' => 'Instagram Business Profile',
-        'threads' => 'Threads Business Profile',
-        'fb' => 'Facebook Business Profile',
-        'maps' => 'Find us on Google Maps',
-        'email' => 'Business Email',
-    ],
-
     // Me
     'me' => [
-        'title' => 'Personal',
-
         'gpg' => 'GPG key',
         'pm' => 'Contact me via ProtonMail',
         'pserv' => 'Contact me via my email server',
--- lang/it/contact.php
+++ lang/it/contact.php
@@ -5,19 +5,8 @@ return [
     'title' => 'Contattami',
     'desc' => 'Scopri come contattarmi.',
 
-    // Salonia Infrastrutture Digitali
-    'sid' => [
-        'ig' => 'Profilo Instagram Aziendale',
-        'threads' => 'Profilo Threads Aziendale',
-        'fb' => 'Profilo Facebook Aziendale',
-        'maps' => 'Trovaci su Google Maps',
-        'email' => 'Email Aziendale',
-    ],
-
     // Me
     'me' => [
-        'title' => 'Personale',
-
         'gpg' => 'Chiave GPG',
         'pm' => 'Contattami tramite ProtonMail',
         'pserv' => 'Contattami tramite il mio server email',
